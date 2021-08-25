2bb61c11ea1b409427e2256d908019f184fc49a9
GANAPATHI MARPINA <mgpathi18888@gmail.com>
2021-08-25 10:55:23 +0530

    added login method

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class LoginService {
   isAuthenticated = false;
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  loginUser() {
+    return this.http
+      .get('https://testdev.org/loginuser');
+     
+  }
 }
